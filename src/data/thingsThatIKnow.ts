ed8e5c9c3443e47b9285d7fd6c6e8747aeea415a
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-11-03 20:23:44 +0100

    Install dev dependencies and run server

---
+++ src/data/thingsThatIKnow.ts
@@ -0,0 +1,11 @@
+import type { ThingThatiKnow } from "../types/types";
+
+const thingsThatiKnow: ThingThatiKnow = {
+  things: [
+    { subject: "scss", id: 1 },
+    { subject: "typescript", id: 2 },
+    { subject: "react", id: 3 },
+  ],
+};
+
+export default thingsThatiKnow;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import thingsThatiKnow from "./data/thingsThatIKnow.js";
 
+dotenv.config();
+
+const { log } = console;
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.get("/things", (req, res) => {
+  res.status(200).json(thingsThatiKnow);
+});
+
+app.listen(port, () => {
+  log("Server is listening");
+});
